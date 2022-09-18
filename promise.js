0c70e3fffb0224de85ba551da41de39a14fc12c3
dwihosanna <45345952+dwihosanna@users.noreply.github.com>
2022-09-18 19:59:18 +0700

    membuat fungsi counting emotion

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const theaters = [...theaterIXX, ...theaterVGC];
+  let count = 0;
+  theaters.forEach((item) => {
+    if(item.hasil == emotion){
+      count++;
+    }
+  })
+  return count;
+};
 
 module.exports = {
   promiseOutput,
